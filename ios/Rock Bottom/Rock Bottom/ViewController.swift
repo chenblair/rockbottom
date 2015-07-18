1d4b7a45e0f8bc2605d3f2de5c887775c8871bdc
Jerry Chen <jerrychendev@gmail.com>
2015-07-18 15:48:57 -0700

    organized folders, created ios project

---
+++ ios/Rock Bottom/Rock Bottom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rock Bottom
+//
+//  Created by Jerry Chen on 7/18/15.
+//  Copyright (c) 2015 Jerry Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
