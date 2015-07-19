c7e26fffd90ffcb369e5b5a4615c9d3743ef71dc
unbrace3 <unbrace3@gmail.com>
2015-07-19 06:30:45 -0700

    Swipes
    swiping changes stories
    now we wait form william to wake up

--- android/RockBottom/app/src/main/java/mycompany/rockbottom/ReadStory.java
+++ android/RockBottom/app/src/main/java/mycompany/rockbottom/ReadStory.java
@@ -26,12 +26,12 @@ import org.apache.http.client.methods.*;
 public class ReadStory extends ActionBarActivity {
 
     private float x1,x2;
-    static String storyText;
+    static String storyText,idArray;
     //boolean hasRun=false;
     static final int MIN_DISTANCE = 150;
-    int arraySize;
+    int arraySize,counter=0;
     public TextView display;
-    String[] ids;
+    String[] ids=new String[100];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +40,10 @@ public class ReadStory extends ActionBarActivity {
         display=(TextView)findViewById(R.id.textDisplay);
         try {
             for (;;) {
-                if (storyText == null) {
+                if (idArray == null) {
                     display.setText("Loading...");
                 } else {
-                    JSONObject jObject = new JSONObject(storyText);
+                    JSONObject jObject = new JSONObject(idArray);
                     //JSONArray jArray = jObject.getJSONArray("ids");
                     JSONArray jArray =jObject.getJSONArray("ids");
                     arraySize=jArray.length();
@@ -53,12 +53,94 @@ public class ReadStory extends ActionBarActivity {
                     break;
                 }
             }
+            displayStory();
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+    }
+    public void displayStory()
+    {
+        try {
+            sendStoryRequest(ids[counter]);
+            for (;;) {
+                if (storyText == null) {
+                    display.setText("Loading...");
+                } else {
+                    JSONObject jObject = new JSONObject(storyText);
+                    String aJsonString = jObject.getString("body");
+                    display.setText(aJsonString);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void sendStoryRequest(final String a) {
+
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String website = "http://rockbottom.ml:8888/story/"+a;
+                BufferedReader in;
+
+                HttpClient httpClient = new DefaultHttpClient();
+
+                // In a POST request, we don't pass the values in the URL.
+                //Therefore we use only the web page URL as the parameter of the HttpPost argument
+                HttpGet request = new HttpGet(website);
+
+                try {
+
+                    try {
+                        HttpResponse response = httpClient.execute(request);
+                        in = new BufferedReader(new InputStreamReader(
+                                response.getEntity().getContent()));
+
+                        StringBuilder stringBuilder = new StringBuilder();
+
+
+                        String bufferedStrChunk = null;
+                        while ((bufferedStrChunk = in.readLine()) != null) {
+                            stringBuilder.append(bufferedStrChunk);
+                        }
+                        storyText = stringBuilder.toString();
+                        return stringBuilder.toString();
+
+                    } catch (ClientProtocolException cpe) {
+                        System.out.println("First Exception caz of HttpResponese :" + cpe);
+                        cpe.printStackTrace();
+                    } catch (IOException ioe) {
+                        System.out.println("Second Exception caz of HttpResponse :" + ioe);
+                        ioe.printStackTrace();
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + e);
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+
+                if (result.equals("working")) {
+                    //Toast.makeText(getApplicationContext(), "HTTP POST is working...", Toast.LENGTH_LONG).show();
+                } else {
+                    //Toast.makeText(getApplicationContext(), "Invalid POST req...", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute();
     }
     public static void sendGetRequest() {
 
@@ -76,28 +158,12 @@ public class ReadStory extends ActionBarActivity {
                 //Therefore we use only the web page URL as the parameter of the HttpPost argument
                 HttpGet request = new HttpGet(website);
 
-                // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
-                //uniquely separate by the other end.
-                //To achieve that we use BasicNameValuePair
-                //Things we need to pass with the POST request
-
                 try {
-                    // UrlEncodedFormEntity is an entity composed of a list of url-encoded pairs.
-                    //This is typically useful while sending an HTTP POST request.
-                    //UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairList);
 
                     try {
                         HttpResponse response = httpClient.execute(request);
                         in = new BufferedReader(new InputStreamReader(
                                 response.getEntity().getContent()));
-
-                        // According to the JAVA API, InputStream constructor do nothing.
-                        //So we can't initialize InputStream although it is not an interface
-                        //InputStream inputStream = httpResponse.getEntity().getContent();
-
-                        //InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-                        //BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
                         StringBuilder stringBuilder = new StringBuilder();
 
@@ -106,7 +172,7 @@ public class ReadStory extends ActionBarActivity {
                         while((bufferedStrChunk = in.readLine()) != null){
                             stringBuilder.append(bufferedStrChunk);
                         }
-                        storyText=stringBuilder.toString();
+                        idArray=stringBuilder.toString();
                         return stringBuilder.toString();
 
                     } catch (ClientProtocolException cpe) {
@@ -140,7 +206,103 @@ public class ReadStory extends ActionBarActivity {
         SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
         sendPostReqAsyncTask.execute();
     }
+   /* private void sendPostRequest(String givenUsername, String givenPassword,String rate) {
 
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String>{
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String paramUsername = params[0];
+                String paramPassword = params[1];
+                String rate=params[2];
+                String website="http://rockbottom.ml:8888/story/"+"";
+                BufferedReader in;
+
+                HttpClient httpClient = new DefaultHttpClient();
+
+                // In a POST request, we don't pass the values in the URL.
+                //Therefore we use only the web page URL as the parameter of the HttpPost argument
+                HttpPost httpPost = new HttpPost(website);
+                //HttpGet request = new HttpGet(website);
+
+                // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
+                //uniquely separate by the other end.
+                //To achieve that we use BasicNameValuePair
+                //Things we need to pass with the POST request
+                BasicNameValuePair usernameBasicNameValuePair = new BasicNameValuePair("userid", paramUsername);
+                BasicNameValuePair passwordBasicNameValuePAir = new BasicNameValuePair("body", paramPassword);
+                BasicNameValuePair rating = new BasicNameValuePair("rating", rate);
+
+                // We add the content that we want to pass with the POST request to as name-value pairs
+                //Now we put those sending details to an ArrayList with type safe of NameValuePair
+                List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+                nameValuePairList.add(usernameBasicNameValuePair);
+                nameValuePairList.add(passwordBasicNameValuePAir);
+                nameValuePairList.add(rating);
+
+                try {
+                    // UrlEncodedFormEntity is an entity composed of a list of url-encoded pairs.
+                    //This is typically useful while sending an HTTP POST request.
+                    UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairList);
+
+                    // setEntity() hands the entity (here it is urlEncodedFormEntity) to the request.
+                    httpPost.setEntity(urlEncodedFormEntity);
+
+                    try {
+                        //HttpResponse response = httpClient.execute(request);
+                        //in = new BufferedReader(new InputStreamReader(
+                        //        response.getEntity().getContent()));
+
+                        // HttpResponse is an interface just like HttpPost.
+                        //Therefore we can't initialize them
+                        HttpResponse httpResponse = httpClient.execute(httpPost);
+
+                        // According to the JAVA API, InputStream constructor do nothing.
+                        //So we can't initialize InputStream although it is not an interface
+                        InputStream inputStream = httpResponse.getEntity().getContent();
+
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        String bufferedStrChunk = null;
+
+                        while((bufferedStrChunk = bufferedReader.readLine()) != null){
+                            stringBuilder.append(bufferedStrChunk);
+                        }
+                        storyText2=stringBuilder.toString();
+                        return stringBuilder.toString();
+
+                    } catch (ClientProtocolException cpe) {
+                        System.out.println("First Exception caz of HttpResponese :" + cpe);
+                        cpe.printStackTrace();
+                    } catch (IOException ioe) {
+                        System.out.println("Second Exception caz of HttpResponse :" + ioe);
+                        ioe.printStackTrace();
+                    }
+
+                } catch (UnsupportedEncodingException uee) {
+                    System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + uee);
+                    uee.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+
+                if(result.equals("working")){
+                    //Toast.makeText(getApplicationContext(), "HTTP POST is working...", Toast.LENGTH_LONG).show();
+                }else{
+                    //Toast.makeText(getApplicationContext(), "Invalid POST req...", Toast.LENGTH_LONG).show();
+                }
+            }
+        }*/
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
@@ -159,12 +321,16 @@ public class ReadStory extends ActionBarActivity {
                     if (x2 > x1)
                     {
                         Toast.makeText(this, "Left to Right swipe [Next]", Toast.LENGTH_SHORT).show ();
+                        counter++;
+                        displayStory();
                     }
 
                     // Right to left swipe action
                     else
                     {
                         Toast.makeText(this, "Right to Left swipe [Previous]", Toast.LENGTH_SHORT).show ();
+                        counter++;
+                        displayStory();
                     }
 
                 }
